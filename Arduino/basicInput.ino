10df15c17a350e201e91b632bd856e691bd0d012
Plush04 <piyushagar04@gmail.com>
2026-07-30 13:10:22 -0400

    used input

---
+++ Arduino/basicInput.ino
@@ -0,0 +1,18 @@
+// practicing inputting into the arduino
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("The program started");
+  Serial.println("Enter your favorite INTEGER number");
+
+}
+
+void loop() {
+  if (Serial.available()){
+    int favNumber = Serial.parseInt();
+    Serial.println("You entered: ");
+    Serial.println(favNumber);
+
+  }
+
+}
