417be3ae4f539799c5fc4a3a542325a43a52eed7
Plush <piyushagar04@gmail.com>
2026-08-06 19:48:21 -0400

    Reorganize repo and remove the project

--- CompEngProjects/ParkingAssistant/Button.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUTTON_H
-#define BUTTON_H
-
-#include <Arduino.h>
-
-void setupButton();
-void readButton(bool &wantsBuzzer);
-
-#endif
--- CompEngProjects/ParkingAssistant/OLED.h
+++
@@ -1,9 +0,0 @@
-#ifndef OLED_H
-#define OLED_H
-#include <Arduino.h>
-
-void setupDisplay();
-
-void updateDisplay(int distance, String parkingStatus, bool wantsBuzzer);
-
-#endif
